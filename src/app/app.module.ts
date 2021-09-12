a77cca564f52f5e3b1e464731e4ca94bf1f7c582
cjf0026 <cjf0026@mix.wvu.edu>
2021-09-12 04:23:59 -0400

    9/14/21

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { TaskListComponent } from './task-list/task-list.component';
+import { LayoutComponent } from './layout/layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SidebarComponent,
+    TaskListComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule
